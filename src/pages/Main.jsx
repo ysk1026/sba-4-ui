acfe7d20262794c76622d06d7eb425239eec935d
Kim <fkqoseka@gmail.com>
2020-10-13 11:50:32 +0900

    11:50

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -8,6 +8,9 @@ const Main = () => <>
                 <Switch>
                     <Route exact path="/" component={Home}/>
                     <Route path="/about" component={About}/>
+                    <Redirect from = {"/history"} to = {"/about/history"}/>
+                    <Redirect from = {"/services"} to = {"/about/services"}/>
+                    <Redirect from = {"/location"} to = {"/about/location"}/>
                     <Route path="/contact" component={Contact}/>
                     <Route path="/events" component={Events}/>
                     <Route path="/products" component={Products}/>
