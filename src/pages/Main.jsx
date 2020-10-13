79838d053cb2ddfcb65ad409f965da533755a40f
Kim <fkqoseka@gmail.com>
2020-10-13 11:14:50 +0900

    first commit

---
+++ src/pages/Main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {About, Contact, Events, Home, Products, Error} from '../components'
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
+
+const Main = () => <>
+        <BrowserRouter>
+            <div className="main">
+                <Switch>
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/about" component={About}/>
+                    <Route path="/contact" component={Contact}/>
+                    <Route path="/events" component={Events}/>
+                    <Route path="/products" component={Products}/>
+                    <Route component={Error}/>
+                </Switch>
+            </div>
+        </BrowserRouter>
+    </>
+
+export default Main
